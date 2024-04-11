3c1e3b9169ed17c2447ccd73c190653c30c98f2a
kurt0cougar <kkleber000@gmail.com>
2024-04-11 05:48:13 +0200

    Add files via upload

---
+++ text_normalization.py
@@ -0,0 +1,37 @@
+from pynini.lib import pynutil
+import pynini
+import nemo_text_processing
+from pynini.lib import pynutil
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Normalize and print a text argument provided by the user.')
+
+parser.add_argument('-t','--text', type=str, help='The text to normalize.')
+
+args = parser.parse_args()
+
+def apply_fst(text, fst):
+    try:
+        return pynini.shortestpath(text @ fst).string()
+
+    except pynini.FstOpError:
+        print(f"Error: no valid output with given input: '{text}'")
+PATH="."
+text = args.text
+classify_far_file = os.path.join(PATH, "tokenize_and_classify.far")
+verbalize_far_file = os.path.join(PATH, "verbalize.far")
+
+classify = pynini.Far(classify_far_file, mode="r")["TOKENIZE_AND_CLASSIFY"]
+verbalize = pynini.Far(verbalize_far_file,mode="r")["ALL"]
+
+classes = apply_fst(text,classify)
+print(classes)
+print("\n====================================\n")
+print("we are here")
+try:
+    print(apply_fst(classes,verbalize))
+    print("\n******************************************\n")
+    print("and then we are here")
+except Exception as E:
+    print(E)
