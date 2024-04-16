44cedd6ab6f2844ffaa35209783378e1dff584fa
kurt0cougar <kkleber000@gmail.com>
2024-04-16 06:40:36 +0200

    Update text_normalization.py

--- text_normalization.py
+++ text_normalization.py
@@ -10,6 +10,18 @@ parser = argparse.ArgumentParser(description='Normalize and print a text argumen
 parser.add_argument('-t','--text', type=str, help='The text to normalize.')
 
 args = parser.parse_args()
+import re
+def normalize_spaces(text):
+    # Replace occurrences of one or more spaces with a single space
+    normalized_text = re.sub(r'\s+', ' ', text)
+    return normalized_text
+
+def compress_consecutive_spaced_na(text):
+
+    pattern = r"(?:\b|(?<=\s))(na )+na(?=\s|$)"
+    # Replace found patterns with a single " na "
+    result = re.sub(pattern, ' na ', text)
+    return result
 
 def apply_fst(text, fst):
     try:
@@ -25,15 +37,18 @@ verbalize_far_file = os.path.join(PATH, "verbalize.far")
 classify = pynini.Far(classify_far_file, mode="r")["TOKENIZE_AND_CLASSIFY"]
 verbalize = pynini.Far(verbalize_far_file,mode="r")["ALL"]
 
-classes = apply_fst(text,classify)
-# print("original: ",classes)
-# print("\n====================================\n")
 # classes = classes.replace("'"," ")
-# print("processed: ",classes)
-# print("\nverbalizing")
+#print("processed: ",classes)
+#print("\nverbalizing")
 try:
-    print(apply_fst(classes,verbalize))
+    classes = apply_fst(text,classify)
+
+    verbalized_text = apply_fst(classes,verbalize)
+    normalized_text = normalize_spaces(verbalized_text)
+    compressed_text = compress_consecutive_spaced_na(normalized_text)
+    print(normalize_spaces(compressed_text))
     # print("\n******************************************\n")
     # print("and then we are here")
 except Exception as E:
-    print(E)
+    print("Failed to process the string........")
+    print(text)
